02c50ecd774bed9d2e6175a213a57e1b644785da
Stefan Szasz <paichpresident@gmail.com>
2016-10-18 15:31:02 +0200

    Add some stuff

--- MyWebApp/MyWebApp/Startup.cs
+++ MyWebApp/MyWebApp/Startup.cs
@@ -8,7 +8,8 @@ namespace MyWebApp
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app); 
+	    //HELLO WORLD
         }
     }
 }
